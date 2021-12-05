0703f4d8883f41e9e719f0aa57a46dc3872ec8f7
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2021-12-05 10:12:31 -0300

    Finish add entities

--- iParty.Business/Models/Orders/Order.cs
+++ iParty.Business/Models/Orders/Order.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using iParty.Business.Models.Addresses;
+using iParty.Business.Models.PaymentPlans;
+using iParty.Business.Models.People;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,19 @@ namespace iParty.Business.Models.Orders
 {
     public class Order: Entity
     {
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public Person Supplier { get; set; }
+        public Person Customer { get; set; }
+        public Address ShippingAddress { get; set; }
+        public decimal Freight { get; set; }
+        public decimal ItemsTotal { get; set; }
+        public decimal OrderTotal { get; set; }
+        public PaymentPlan PaymentPlan { get; set; }
+        public string Notes { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime PartyDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
     }
 }
--- iParty.Business/Models/Orders/OrderItem.cs
+++ iParty.Business/Models/Orders/OrderItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iParty.Business.Models.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace iParty.Business.Models.Orders
 {
     public class OrderItem: Entity
     {
-
+        public Order Order { get; set; }
+        public Item Item  { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total { get; set; }
     }
 }
