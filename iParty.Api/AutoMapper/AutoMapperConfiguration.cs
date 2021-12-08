8979d641025ec736fd85d9b3db6fd29da04eb0c1
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2021-12-07 21:02:02 -0300

    Add AutoMapper configuration

---
+++ iParty.Api/AutoMapper/AutoMapperConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iParty.Api.AutoMapper
+{
+    public class AutoMapperConfiguration
+    {
+        public AutoMapperConfiguration()
+        {
+            //CreateMap<Supplier, SupplierViewModel>().ReverseMap();
+        }
+    }
+}
