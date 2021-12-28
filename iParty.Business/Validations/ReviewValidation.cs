2d7d15eff874e2cd691ca6c2536b2a788a1b94e2
haitineto <haitineto007@hotmail.com>
2021-12-28 14:32:41 -0300

    retirando update da notificação

--- iParty.Business/Validations/ReviewValidation.cs
+++ iParty.Business/Validations/ReviewValidation.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using iParty.Business.Interfaces.Validations;
 using iParty.Business.Models.Review;
+using System;
 
 namespace iParty.Business.Validations
 {
@@ -8,8 +9,7 @@ namespace iParty.Business.Validations
     {
         public ReviewValidation()
         {
-            RuleFor(p => p.Date).NotNull().WithMessage("A data da avaliação não foi informada.");
-            RuleFor(p => p.Time).NotNull().WithMessage("O tempo para avaliação não foi informado.");
+            RuleFor(p => p.Date).GreaterThan(DateTime.MinValue).WithMessage("A data da avaliação não foi informada.");
             RuleFor(p => p.Description).NotEmpty().WithMessage("A descrição da avaliação não foi informada.");
             RuleFor(p => p.Stars).InclusiveBetween(1, 5).WithMessage("As quantidades de estrelas precisam estar entre 1 e 5.");                                 
         }
