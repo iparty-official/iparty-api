6f987c0074a095c354edd8f450e9e906aabfde16
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2021-12-29 19:24:26 -0300

    Remove order controller from the project

--- iParty.Api/Views/Orders/OrderItemView.cs
+++ iParty.Api/Views/Orders/OrderItemView.cs
@@ -1,6 +1,12 @@
-﻿namespace iParty.Api.Views.Orders
+﻿using iParty.Api.Views.Items;
+
+namespace iParty.Api.Views.Orders
 {
     public class OrderItemView : View
     {
+        public ItemSummarizedView Item { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total { get; set; }
     }
 }
--- iParty.Api/Views/Orders/OrderView.cs
+++ iParty.Api/Views/Orders/OrderView.cs
@@ -1,6 +1,26 @@
-﻿namespace iParty.Api.Views.Orders
+﻿using iParty.Api.Views.Addresses;
+using iParty.Api.Views.PaymentPlans;
+using iParty.Api.Views.People;
+using iParty.Business.Models.Orders;
+using System;
+using System.Collections.Generic;
+
+namespace iParty.Api.Views.Orders
 {
     public class OrderView : View
     {
+        public DateTime DateTime { get; set; }
+        public PersonSummarizedView Supplier { get; set; }
+        public PersonSummarizedView Customer { get; set; }
+        public AddressView ShippingAddress { get; set; }
+        public decimal Freight { get; set; }
+        public decimal ItemsTotal { get; set; }
+        public decimal OrderTotal { get; set; }
+        public PaymentPlanView PaymentPlan { get; set; }
+        public string Notes { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime PartyDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public List<OrderItemView> Items { get; set; }
     }
 }
