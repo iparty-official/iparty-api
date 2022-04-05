c3acd16c29c4b8b845e8ae8a40e0f2794ecca44d
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2022-04-04 23:25:34 -0300

    Finish message swagger description

--- iParty.Api/Controllers/Messages/MessageConstant.cs
+++ iParty.Api/Controllers/Messages/MessageConstant.cs
@@ -4,16 +4,16 @@
     {
         public const string Tag = "Message";
 
-        public const string CreateSummary = "Create a message in the system.";
-        public const string UpdateSummary = "Update a message in the system.";
-        public const string DeleteSummary = "Remove a message from the system.";
-        public const string GetByIdSummary = "Get a specific message from the system.";
-        public const string GetAllSummary = "Get all the messages from the system.";
+        public const string CreateSummary = "It creates a message from a customer to a supplier or vice versa.";
+        public const string UpdateSummary = "It updates a message.";
+        public const string DeleteSummary = "It removes a message.";
+        public const string GetByIdSummary = "It gets a specific message.";
+        public const string GetAllSummary = "It gets all the messages.";
 
-        public const string CreateDescription = "Honestly, this metohod shouldn't exist. Because the cities must be supplied by the internal team of the system.";
-        public const string DeleteDescription = "Please, don't call this method. I'm begging you.";
-        public const string UpdateDescription = "Please, don't call this method. I'm begging you.";
-        public const string GetByIdDescription = "If you want to see details of the customer or supplier city, use this method.";
-        public const string GetAllDescription = "If you want to create a search field that allows the user to find their own city, use this method.";
+        public const string CreateDescription = "Eventually, a customer will send a question to a supplier (or vice versa). Your app should use this method to send this message. ";
+        public const string DeleteDescription = "Once it was is sent, a message shouldn't be removed. So, this method doesn't make sense.";
+        public const string UpdateDescription = "Once it was is sent, a message shouldn't be updated. So, this method doesn't make sense.";
+        public const string GetByIdDescription = "Your app will probably have a screen to show to message to the user and this method will help you with it.";
+        public const string GetAllDescription = "Your app will probably have a screen to list all the user's messages and this method will help you with it.";
     }
 }
